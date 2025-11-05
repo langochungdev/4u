41d9726add8f37d8fef1930b42552b8e829b226c
langochungdev <langochungdev@gmail.com>
2025-11-05 19:26:24 +0700

    feat: add galaxy

---
+++ src/pages/templates/galaxy/galaxy.config.ts
@@ -0,0 +1,11 @@
+import type { TemplateConfig } from '@/config/templates';
+
+const config: TemplateConfig = {
+    maxImages: 4,
+    maxVideos: 0,
+    maxAudios: 0,
+    maxContent: 4,
+    templateName: 'galaxy'
+};
+
+export default config;
