7cb450222487525e8aa02727141ebfe9989f04e1
Dương Đình Trác - Lab1 <duongdinhtrac0103@gmail.com>
2025-11-08 22:48:51 +0700

    feat: fix folder name

--- src/pages/templates/AnniversaryLove/anni.config.ts
+++ src/pages/templates/AnniversaryLove/anni.config.ts
@@ -1,7 +1,7 @@
 import type { TemplateConfig } from '@/config/templates';
 
 const config: TemplateConfig = {
-  templateName: 'Anniversary',
+  templateName: 'AnniversaryLove',
   maxImages: 5,
   maxVideos: 0,
   maxAudios: 0,
