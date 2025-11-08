e07874a48bfd83852cfa253fc6c72568a9d6fb9f
Dương Đình Trác - Lab1 <duongdinhtrac0103@gmail.com>
2025-11-08 20:11:49 +0700

    feat: add Anni template with interactive letter submission

---
+++ src/pages/templates/Anni/anni.config.ts
@@ -0,0 +1,11 @@
+import type { TemplateConfig } from '@/config/templates';
+
+const config: TemplateConfig = {
+  templateName: 'demoTrac',
+  maxImages: 5,
+  maxVideos: 0,
+  maxAudios: 0,
+  maxContent: 2
+};
+
+export default config;
