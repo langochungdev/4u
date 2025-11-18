3c1aceb04c857166bfa304092e386fc5c12132b2
Dương Đình Trác - Lab1 <duongdinhtrac0103@gmail.com>
2025-11-18 10:13:59 +0700

    feat: add 20-11 notebook template config

---
+++ frontend/src/pages/templates/20-11/notebook-for-2011/config.ts
@@ -0,0 +1,22 @@
+import type { TemplateConfig } from '@/config/templates';
+import demo from './demo.png';
+
+const config: TemplateConfig = {
+    title: 'Sách tri lưu giữ kỷ niệm 20-11 ',
+    description: 'Hãy để lại những lời chúc ý nghĩa và hình ảnh đẹp nhất dành tặng thầy cô nhân ngày Nhà Giáo Việt Nam 20-11 với mẫu sổ tay tri ân này!',
+    createdBy: 'DuongDinhTrac',
+    maxImages: 5,
+    maxVideos: 0,
+    maxAudios: 1,
+    maxContent: 1,
+    contentPlaceholders: [
+        'Nhập Nội Dung Thư'
+    ],
+
+    demoId: 'demo',
+    thumbnail: demo,
+    thumbnailType: 'image',
+    templateName: new URL('.', import.meta.url).pathname.split('/').pop() || ''
+};
+
+export default config;
