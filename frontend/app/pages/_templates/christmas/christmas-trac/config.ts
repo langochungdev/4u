a1c5ce2db6c42aa87fecccb98cd1cef5fdee35bf
Dương Đình Trác - Lab1 <duongdinhtrac0103@gmail.com>
2025-11-23 00:35:12 +0700

    docs: Update Christmas template config
    - Updated title and description
    - Added content placeholders

--- frontend/app/pages/_templates/christmas/christmas-trac/config.ts
+++ frontend/app/pages/_templates/christmas/christmas-trac/config.ts
@@ -2,15 +2,16 @@ import type { TemplateConfig } from '@/config/templates';
 import demo from './demo.webp';
 
 const config: TemplateConfig = {
-    title: 'christmas-trac - Sổ tay Giáng Sinh 2024',
-    description: '',
+    title: 'christmas-tree - Cây thông noel',
+    description: 'Gửi những ảnh giáng sinh đáng yêu kèm lời chúc ấm áp đến người thân và bạn bè của bạn trong dịp lễ hội này!',
     createdBy: 'DuongDinhTrac',
     maxImages: 5,
     maxVideos: 0,
     maxAudios: 1,
     maxContent: 2,
     contentPlaceholders: [
-       
+       'Title lá thư',
+       'Nội dung lời chúc'
     ],
 
     thumbnail: demo,
